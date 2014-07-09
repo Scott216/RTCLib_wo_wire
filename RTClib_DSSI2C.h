3f5cb253001caa206882d26e09d0cfdbf1171ce3
Scott Goldthwaite <scott@goldthwaite.com>
2014-07-09 15:54:51 -0400

    Add TimeSpan class and simple DateTime duration
    Add TimeSpan class and simple DateTime duration arithmetic support.

--- RTClib_DSSI2C.h
+++ RTClib_DSSI2C.h
@@ -11,19 +11,27 @@ Released to the public domain! Enjoy!
 Simple general-purpose date/time class (no TZ / DST / leap second handling!)
 
 Change Log
-7/9/14 - Use __DATE__ __TIME__ from PROGMEM (this mirrors adafruit change on 2/20/14)
+07/09/14 - Use __DATE__ __TIME__ from PROGMEM (this mirrors Adafruit change on 2/20/14)
+07/09/14 - Add TimeSpan class and simple DateTime duration arithmetic support (mirrors Adafruit change on 6/12/14, see: bit.ly/1ojYKAL)
 
+
+To Do:
+Add square wave output support: http://github.com/adafruit/RTClib/commit/65ae8047d1c3e57bc9f1ccb09018f72cd29b19d4
+Add NVRAM functions  http://github.com/adafruit/RTClib/commit/87624ede6728522d1457905754a6222ca54033b1
 
 */
 
 
 #include <Arduino.h> 
 
+class TimeSpan;
+
 class DateTime {
 public:
     DateTime (uint32_t t =0);
     DateTime (uint16_t year, uint8_t month, uint8_t day,
                 uint8_t hour =0, uint8_t min =0, uint8_t sec =0);
+    DateTime (const DateTime& copy);
     DateTime (const char* date, const char* time);
     DateTime (const __FlashStringHelper* date, const __FlashStringHelper* time);
     uint16_t year() const       { return 2000 + yOff; }
@@ -39,9 +47,34 @@ public:
     // 32-bit times as seconds since 1/1/1970
     uint32_t unixtime(void) const;
 
+    DateTime operator+(const TimeSpan& span);
+    DateTime operator-(const TimeSpan& span);
+    TimeSpan operator-(const DateTime& right);
+    
 protected:
     uint8_t yOff, m, d, hh, mm, ss;
 };
+
+
+// Timespan which can represent changes in time with seconds accuracy.
+class TimeSpan {
+public:
+    TimeSpan (int32_t seconds = 0);
+    TimeSpan (int16_t days, int8_t hours, int8_t minutes, int8_t seconds);
+    TimeSpan (const TimeSpan& copy);
+    int16_t days() const         { return _seconds / 86400L; }
+    int8_t  hours() const        { return _seconds / 3600 % 24; }
+    int8_t  minutes() const      { return _seconds / 60 % 60; }
+    int8_t  seconds() const      { return _seconds % 60; }
+    int32_t totalseconds() const { return _seconds; }
+
+    TimeSpan operator+(const TimeSpan& right);
+    TimeSpan operator-(const TimeSpan& right);
+
+protected:
+    int32_t _seconds;
+};
+
 
 // RTC based on the DS1307 chip connected via I2C and the Wire library
 class RTC_DS1307 {
